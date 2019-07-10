5dba49acaa59b31c96f49ecedc612f961c674a49
lewisbaafi <lewisbaafi@gmail.com>
2019-07-10 16:11:06 +0000

    lewis.py

---
+++ lewis.py
@@ -0,0 +1,13 @@
+print ("hello")
+age = input()
+page = input()
+if age < page:
+    print ("that's cool")
+elif age > page:
+    print("that's normal")
+print (int(age) + int(page))
+result = int(age) + int(page)
+print (result)
+while result < 100
+result = result + 1
+print(result)
